74d31eb5fe180c7d979608e8625491c3215436cb
hisa <hisayuki.k8@gmail.com>
2022-12-25 22:52:10 +0900

    update ssg.tsx

--- pages/ssg.tsx
+++ pages/ssg.tsx
@@ -1,9 +1,9 @@
-import { NextPage } from 'next'
+import { GetStaticProps, NextPage, NextPageContext } from 'next'
 import Head from 'next/head'
 
-type Props = {}
+type Props = { message: string }
 
-const SSG: NextPage<Props> = () => {
+const SSG: NextPage<Props> = ({ message }) => {
   return (
     <div>
       <Head>
@@ -12,9 +12,21 @@ const SSG: NextPage<Props> = () => {
       </Head>
       <main>
         <p>このページは静的サイト生成によってビルド時に生成されたページです</p>
+        <p>{message}</p>
       </main>
     </div>
   )
+}
+
+// getStaticPropsはビルド時に実行される
+// getStaticPropsはexportする必要があり、asyncと共に定義する必要がある
+export const getStaticProps: GetStaticProps<Props> = async (context) => {
+  const dateString = new Date().toLocaleTimeString()
+  const message = `${dateString}にgetStaticPropsが実行されました`
+  console.log(message)
+  return {
+    props: { message },
+  }
 }
 
 export default SSG
