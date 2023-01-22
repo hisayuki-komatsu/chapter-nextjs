692caf30df4362e371c573a12c1d796fd0c9fe44
hisa <hisayuki.k8@gmail.com>
2023-01-22 20:48:18 +0900

    update document tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { DocumentContext } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-const Document = () => {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getIntialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+
+      // initialPropsに加えて、styleを追加して返す
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
 }
-
-export default Document
