1fc0db81fcdad36e5c74be42e8194b52b870b031
hisa <hisayuki.k8@gmail.com>
2023-01-23 12:52:48 +0900

    fix sb setting

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,6 @@
 module.exports = {
-  stories: [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  typescript: { reactDocgen: false },
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
