a2d03e28d7d3586ee4e8984a4f5f86376b13fb95
hisa <hisayuki.k8@gmail.com>
2023-03-04 23:50:09 +0900

    feat: add husky

---
+++ .lintstagedrc.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `yarn lint:fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
+const buildPrettierCommand = (filenames) =>
+  `yarn format --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
+const buildStylelintCommand = () => `yarn stylelint:fix`
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [
+    buildEslintCommand,
+    buildPrettierCommand,
+    buildStylelintCommand,
+  ],
+}
